b7525192abc69e04388b6b8dc4ffaada83c39653
jyfernandez <jydfernandez@gmail.com>
2021-08-05 16:48:46 +0800

    Update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,31 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Dish } from './modules/dish/model/dish.model';
 import { DishModule } from './modules/dish/dish.module';
 import { OrderModule } from './modules/order/order.module';
-import { Order } from './modules/order/model/order.model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config/config.schema';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb://localhost/popina',
-      synchronize: true,
-      useUnifiedTopology: true,
-      entities: [Dish, Order],
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mongodb',
+          autoLoadEntities: true,
+          synchronize: true,
+          useUnifiedTopology: true,
+          url: configService.get('DB_URL'),
+        };
+      },
+    }),
+
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
